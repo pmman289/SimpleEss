bf4e8d235f5eec314c9537a7c8d8c0750dc605c8
pmman <me@pmman.tech>
2026-01-21 22:35:35 +0800

    引入jackson，将/home与/sethome改造成使用数据库

---
+++ src/main/java/tech/pmman/pojo/Location.java
@@ -0,0 +1,34 @@
+package tech.pmman.pojo;
+
+import com.hypixel.hytale.math.vector.Vector3d;
+import com.hypixel.hytale.math.vector.Vector3f;
+import lombok.Data;
+import tech.pmman.util.JsonUtil;
+
+@Data
+public class Location {
+    private Vector3d position;
+    private Vector3f rotation;
+
+    public Location(Vector3d position, Vector3f rotation) {
+        this.position = position;
+        this.rotation = rotation;
+    }
+
+    public Location() {
+    }
+
+    /**
+     * 将json字符串转换为Location，约定合法的字符串为"p"
+     *
+     * @param json json
+     * @return 对象
+     */
+    public static Location fromString(String json) {
+        return JsonUtil.fromJson(Location.class, json);
+    }
+
+    public String toJson() {
+        return JsonUtil.toJson(this);
+    }
+}
--- src/main/java/tech/pmman/service/HomeService.java
+++ src/main/java/tech/pmman/service/HomeService.java
@@ -1,45 +1,58 @@
 package tech.pmman.service;
 
 import com.hypixel.hytale.component.Ref;
-import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.Universe;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
-import tech.pmman.ConfigManager;
+import tech.pmman.DbManager;
+import tech.pmman.dao.mapper.PlayerHomeMapper;
 import tech.pmman.pojo.PlayerLocationEntry;
+import tech.pmman.pojo.db.PlayerHomeEntry;
 import tech.pmman.util.MessageTool;
 import tech.pmman.util.PlayerTool;
 
 import javax.annotation.Nonnull;
-import java.util.Map;
+import java.util.List;
 import java.util.UUID;
 
 public class HomeService {
-    public static void goHome(String homeName, @Nonnull CommandContext commandContext, @Nonnull Store<EntityStore> store,
-                              @Nonnull Ref<EntityStore> ref, @Nonnull PlayerRef playerRef, @Nonnull World world){
+    public static void goHome(String homeName, @Nonnull CommandContext commandContext,
+                              @Nonnull Ref<EntityStore> ref, @Nonnull PlayerRef playerRef) {
         String uuid = playerRef.getUuid()
                                .toString();
-        Map<String, PlayerLocationEntry> homeMap = ConfigManager.PLAYER_HOME_DATA.get()
-                                                                                 .getHomeMap(uuid);
-        if (!homeMap.containsKey(homeName)) {
-            MessageTool.sendPluginMessage(commandContext, Message.translation("simpleEssCommand.home.nohome")
-                                                                 .param("homeName", homeName));
-            return;
-        }
-        PlayerLocationEntry location = homeMap.get(homeName);
-        // 判断世界是否在线
-        World targetWorld = Universe.get()
-                                    .getWorld(UUID.fromString(location.getWorldUUID()));
-        if (targetWorld == null || !targetWorld.isAlive()) {
-            MessageTool.sendPluginMessage(playerRef, Message.translation("simpleEssCore.teleport.targetWorldNotOnline"));
-            return;
-        }
-        PlayerTool.teleportPlayer(ref, targetWorld, location.getPosition(), location.getRotation());
-        PlayerTool.recordPlayerTransformHistory(playerRef.getUuid()
-                                                         .toString(), location);
-        MessageTool.sendPluginMessage(commandContext, Message.translation("simpleEssCommand.home.execute"));
+        DbManager.getInstance()
+                 .get()
+                 .useExtension(PlayerHomeMapper.class, dao -> {
+                     List<PlayerHomeEntry> homes = dao.queryPlayerHomes(uuid);
+                     PlayerHomeEntry targetHome = homes.stream()
+                                                       .filter(h -> h.getHomeName()
+                                                                     .equals(homeName))
+                                                       .findFirst()
+                                                       .orElse(null);
+                     if (targetHome == null) {
+                         MessageTool.sendPluginMessage(commandContext, Message.translation("simpleEssCommand.home.nohome")
+                                                                              .param("homeName", homeName));
+                         return;
+                     }
+                     // 判断世界是否在线
+                     World targetWorld = Universe.get()
+                                                 .getWorld(UUID.fromString(targetHome.getWorldUUID()));
+                     if (targetWorld == null || !targetWorld.isAlive()) {
+                         MessageTool.sendPluginMessage(playerRef, Message.translation("simpleEssCore.teleport.targetWorldNotOnline"));
+                         return;
+                     }
+                     PlayerTool.teleportPlayer(ref, targetWorld, targetHome.getLocationObj()
+                                                                           .getPosition(), targetHome.getLocationObj()
+                                                                                                     .getRotation());
+                     PlayerTool.recordPlayerTransformHistory(playerRef.getUuid()
+                                                                      .toString(), new PlayerLocationEntry(targetHome.getWorldUUID(),
+                             targetHome.getLocationObj()
+                                       .getPosition(), targetHome.getLocationObj()
+                                                                 .getRotation()));
+                     MessageTool.sendPluginMessage(commandContext, Message.translation("simpleEssCommand.home.execute"));
+                 });
     }
 }
